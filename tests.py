a06077c8e90fa64ada023bf11981980aa165e93a
Alejandro Saucedo <axsauze@gmail.com>
2014-12-06 14:58:15 +0000

    added new test class to test multiple messages

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
 import unittest
-from send import send_message
+from send import send_message, send_multiple_messages
 from twilio import TwilioRestException
 
 # This is a class that will test basic functionality of the send
 # message function that Ali created
-class TestSendPyModule(unittest.TestCase):
+class TestSendMessage(unittest.TestCase):
 
     def test_valid_params(self):
     	ret_msg = send_message(body="Test 1", phone="07438483904")
-        self.assertTrue(ret_msg is not "failed")
+        self.assertTrue(ret_msg is "queued")
 
     def test_null_phone(self):
     	ret_msg = send_message(body="Test 2")
@@ -16,19 +16,26 @@ class TestSendPyModule(unittest.TestCase):
 
     def test_null_body(self):
     	ret_msg = send_message(phone="07438483904")
-        self.assertTrue(ret_msg is not "failed")
+        self.assertTrue(ret_msg is "queued")
 
     def test_null_body_and_phone(self):
     	ret_msg = send_message()
-        self.assertTrue(ret_msg is not "failed")
+        self.assertTrue(ret_msg is "queued")
 
     def test_null_empty_body(self):
     	ret_msg = send_message(body="")
-        self.assertTrue(ret_msg is not "failed")
+        self.assertTrue(ret_msg is "queued")
 
     def test_invalid_body(self):
     	ret_msg = send_message(body=100)
-        self.assertTrue(ret_msg is not "failed")
+        self.assertTrue(ret_msg is "queued")
+
+class TestSendMultipleMessages(unittest.TestCase):
+
+	def test_one_valid_message(self):
+		messages = [{ 'body': 'This is a valid body', 'phone': "07438483904" }]
+		ret_arr = send_multiple_messages(messages)
+		self.assertTrue(ret_arr[0]=='queued')
 
 if __name__ == '__main__':
 	print "Running main program..."
