b1eb2ca5f8518b3d6b16264e070f1e0350b03f64
Ali Maleklou <alimaleklou@alis-mbp.home>
2014-12-06 13:29:44 +0000

    this function sends text messages

---
+++ send.py
@@ -0,0 +1,12 @@
+from twilio.rest import TwilioRestClient 
+ 
+# put your own credentials here 
+ACCOUNT_SID = "ACf0027f784ea3bc216836afc2bbc4004d" 
+AUTH_TOKEN = "fbe55d3c4254086da12de6b62f3a5d23" 
+ 
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+
+
+
+def send_message(body=None, phone=None):
+	return False
