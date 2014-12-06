ab51d97b6b95b2a0e1f59eb9a6b74d32400227dc
Alejandro Saucedo <axsauze@gmail.com>
2014-12-06 13:38:51 +0000

    Added test for send message function

--- send.py
+++ send.py
@@ -1,4 +1,5 @@
 from twilio.rest import TwilioRestClient 
+import unittest
  
 # put your own credentials here 
 ACCOUNT_SID = "ACf0027f784ea3bc216836afc2bbc4004d" 
@@ -6,7 +7,21 @@ AUTH_TOKEN = "fbe55d3c4254086da12de6b62f3a5d23"
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
-
-
 def send_message(body=None, phone=None):
 	return False
+
+# This is a function that test basic functionality of the send
+# message function that Ali created
+def test_send_message():
+	print "Testing send message..."
+
+	rCode = send_message()
+
+	print "Successful" if rCode else "[ERROR]"
+
+	return True
+
+
+if __name__ == '__main__':
+	print "Running main program..."
+	test_send_message()
