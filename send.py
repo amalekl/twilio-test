7e9729bf075655fd64b1510a986391dff6877715
Ali Maleklou <alimaleklou@alis-mbp.home>
2014-12-06 14:20:21 +0000

    merging with new tests

--- send.py
+++ send.py
@@ -6,10 +6,17 @@ AUTH_TOKEN = "fbe55d3c4254086da12de6b62f3a5d23"
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
+
 def send_message(body=None, phone=None):
+	the_phone=phone 
+	the_body=body
+	if range(the_phone)!=11:
+		return "invalid phone number"
+	if the_body=None:
+		the_body="Empty Text"
 	ali=client.messages.create(
-		to=phone, 
+		to=the_phone, 
 		from_="+441290211149", 
-		body=body,  
+		body=the_body,  
 	)
 	return ali
